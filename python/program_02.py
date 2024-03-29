386d44445cdf9a45c9ec8f7af49a0dc5a7ea3900
Mihir Chakma <mihirchakmaths@gmail.com>
2024-03-29 22:32:40 +0530

    added program 2

---
+++ python/program_02.py
@@ -0,0 +1,14 @@
+"""
+2. Write a program that calculates the arithmetic mean of two numbers
+
+"""
+
+# promt the user for two numbers 
+number_1 = float(input("Enter first number: "))
+number_2 = float(input("Enter second number: "))
+
+# calculate the arithmetic mean 
+mean = (number_1 + number_2) / 3
+
+# Display the result 
+print(f"Arithmetic mean is {mean:.2f}")
