753337aa5ce9abdec0784b04cda290fe292796db
Mihir Chakma <mihirchakmaths@gmail.com>
2024-04-04 21:24:23 +0530

    update program 03

--- python/program_03.py
+++ python/program_03.py
@@ -11,4 +11,6 @@ grade3 = float(input("Enter the third grade: "))
 # calculate the arithmetic mean 
 mean = (grade1 + grade2 + grade3) / 3
 
-print(f"Arithmetic mean: {mean}")
+rounded_mean = round(mean, 2)
+
+print(f"Arithmetic mean: {rounded_mean}")
